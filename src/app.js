c4a6760094144b7de6f80b815a7852f2d7f9828c
vishalawaken <tsinghvishall@gmail.com>
2025-10-17 15:35:59 +0530

    Implement user data handling with GET and POST routes in app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ const express=require("express");
 
 const app=express();
 
-app.use("/helloo",(req,res)=>
-    {
-        res.send("Hello betaa")
-    })
+// This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send("here is the data of the user")
+})
+
+app.post("/user",(req,res)=>{
+    console.log("save data to the database")
+    res.send("Data succesfully saved")
+})
+
 
 app.use("/test",(req,res)=>
 {
     res.send("Hello from the server")
-})
+});
 
 app.listen(3000,()=>{
     console.log("succesfully listening on port 3000")
